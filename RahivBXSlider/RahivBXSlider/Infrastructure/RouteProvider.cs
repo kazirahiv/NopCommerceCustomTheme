3a10973e49b03702e2e659d379c29423aeb51ba9
Kazi Rahiv <rahivjobs@gmail.com>
2021-05-31 23:57:33 +0600

    Razor Views and Route Provider implementation

---
+++ RahivBXSlider/RahivBXSlider/Infrastructure/RouteProvider.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Nop.Web.Framework.Mvc.Routing;
+
+namespace RahivBXSlider.Infrastructure
+{
+    class RouteProvider : IRouteProvider
+    {
+ 
+
+        public void RegisterRoutes(IEndpointRouteBuilder endpointRouteBuilder)
+        {
+            endpointRouteBuilder.MapControllerRoute("MyCustomPluginRoute", "Plugins/TestMyCustomPlugin/Index",
+                new { controller = "TestMyCustomPlugin", action = "Index" });
+        }
+
+        public int Priority => 0;
+    }
+}
